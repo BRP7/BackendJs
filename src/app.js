98286f0e87a4f1a57ff580ab889714bf1f9dde54
BRP7 <bhumiparmar0007@gmail.com>
2024-06-27 22:35:05 +0530

    updates

--- src/app.js
+++ src/app.js
@@ -15,6 +15,8 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+
+//importing router
 import userRouter from '../routers/user.routes.js'; 
 
 
