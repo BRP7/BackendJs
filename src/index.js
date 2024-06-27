ff87fedd5455e9429de040c0044004c64396ea1b
BRP7 <bhumiparmar0007@gmail.com>
2024-06-27 21:44:12 +0530

    updates

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { app } from './app.js';
 import dotenv from "dotenv";
 dotenv.config({ path: './env' });
 
-//change port number
 const PORT = process.env.PORT || 8012;
 
 // Connect to the database
