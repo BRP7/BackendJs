ffd93d941bc0b6ed6d1b66866d7ab483dee13a75
BRP7 <bhumiparmar0007@gmail.com>
2024-06-29 19:09:37 +0530

    updates

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,10 @@
 import { asyncHandle } from "../../utils/asyncHandle.js";
 
 const registerUser = asyncHandle(async (req, res) => {
+    //get user details from front end
+    //validation - not empty
+    //check if already exist
+    
     res.status(200).json({
         message: "ok"
     });
