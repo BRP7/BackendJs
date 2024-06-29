8c2ee96b85657f0c751447b84e80333a2a4897ea
BRP7 <bhumiparmar0007@gmail.com>
2024-06-29 19:11:45 +0530

    updates

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,7 +6,10 @@ const registerUser = asyncHandle(async (req, res) => {
     //check if already exists: username,email
     //check for images , check for avatar
     //upload them to cloudinary
-    //
+    //create user object - create entry in db
+    //remove password and referesh token field from response
+    //check for user creation
+    //return res
     res.status(200).json({
         message: "ok"
     });
