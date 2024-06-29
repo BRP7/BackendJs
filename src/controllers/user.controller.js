ad8afd3c5fb3cd3fec26800911269c0037aeede0
BRP7 <bhumiparmar0007@gmail.com>
2024-06-29 19:10:34 +0530

    updates

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,8 +3,10 @@ import { asyncHandle } from "../../utils/asyncHandle.js";
 const registerUser = asyncHandle(async (req, res) => {
     //get user details from front end
     //validation - not empty
-    //check if already exist
-    
+    //check if already exists: username,email
+    //check for images , check for avatar
+    //upload them to cloudinary
+    //
     res.status(200).json({
         message: "ok"
     });
