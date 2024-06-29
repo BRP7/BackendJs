f71d914c9a670da41fcb77f18c4f5614763d5825
BRP7 <bhumiparmar0007@gmail.com>
2024-06-29 20:18:01 +0530

    updates

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import { asyncHandle } from "../../utils/asyncHandle.js";
-
+import { ApiError } from "../utils/ApiError.js"
+import { User } from "../models/user.model.js"
 const registerUser = asyncHandle(async (req, res) => {
     //get user details from front end
     //validation - not empty
@@ -12,7 +13,28 @@ const registerUser = asyncHandle(async (req, res) => {
     //return res 
 
     const{fullname , email , username , password} = req.body;
-    console.log("email : ",email);
+    // if(fullname == ''){
+    //     throw new ApiError(400,"Full Name Is Required")
+    // }
+
+    if([fullname,email,username,password].some((field)=>field?.trim() === "")){
+        throw new ApiError(400,"All Fields are required")
+    }
+
+   existedUser = User.findOne({
+        $or: [{username},{email}]
+    })
+
+    if(existedUser){
+        throw new ApiError(400,"User with Eail or Username Exist!")
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    if(!avatarLocalPath){
+        throw new ApiError("Avatar is Required!");
+    }
 
     res.status(200).json({
         message: "ok"
