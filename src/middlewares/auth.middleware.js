e4c1a4917d7dc0104c2175e12f5eb2be272e11db
BRP7 <bhumiparmar0007@gmail.com>
2024-07-02 23:33:33 +0530

    Add User Account Management Endpoints

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,27 +3,27 @@ import { ApiError } from "../../utils/ApiError.js";
 import { asyncHandle } from "../../utils/asyncHandle.js";
 import { User } from "../models/user.models.js";
 
-export const verifyJwt = asyncHandle(async(req,res,next)=>{
-   try {
-    console.log(req.cookies?.accessToken);
-    // const token =  req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
-    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","");
-     if(!token){
-         throw new ApiError(401,"Unauthorized Token")
-     }
- 
-     const decodeToken = await jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
-    const user =  await User.findById(decodeToken?._id).select("-password -refreshToken")
- 
-    if(!user){
-     throw new ApiError(401,"Invalid Access Token")
+export const verifyJwt = asyncHandle(async (req, res, next) => {
+    try {
+        console.log(req.cookies?.accessToken);
+        // const token =  req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
+        if (!token) {
+            throw new ApiError(401, "Unauthorized Token")
+        }
+
+        const decodeToken = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+        const user = await User.findById(decodeToken?._id).select("-password -refreshToken")
+
+        if (!user) {
+            throw new ApiError(401, "Invalid Access Token")
+        }
+
+        req.user = user;
+        next();
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid Token")
+
     }
- 
-    req.user = user;
-    next();
-   } catch (error) {
-    throw new ApiError(401,error?.message || "Invalid Token")
-    
-   }
 
 })
