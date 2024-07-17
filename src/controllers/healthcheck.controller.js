065ac28fc3915efad9cc902b73426ef5464048a1
BRP7 <bhumiparmar0007@gmail.com>
2024-07-17 19:48:25 +0530

    added healthcheck  router

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -1,13 +1,16 @@
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
-
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const healthcheck = asyncHandler(async (req, res) => {
-    //TODO: build a healthcheck response that simply returns the OK status as json with a message
-})
+    const healthStatus = {
+        status: "OK",
+        message: "Server is running smoothly",
+        timestamp: new Date().toISOString()
+    };
+    return res.status(200).json(new ApiResponse(200, healthStatus, "Health check successful"));
+});
 
 export {
     healthcheck
-    }
-    
+};
